02db2038fdb3f56c0cfe71e36fa5f40f2108b25a
IlianPihlajamaa <73794090+IlianPihlajamaa@users.noreply.github.com>
2022-05-07 19:35:16 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,34 @@
 using Test, PermutationSymmetricTensors # This load both the test suite and our MyAwesomePackage
 
-a = rand(SymmetricTensor{Float64, 10, 2})
 
-@test length(a.data) ==  symmetric_tensor_size(10, 2)
+@testset "Creation" begin
+    for T in [Float64, ComplexF32, BigFloat, Bool]
+        for N in [2, 5, 10]
+            for dim in [2, 5, 10]
+                for func in [rand, ones, zeros]
+                    a = func(SymmetricTensor{T, N, dim})
+                    @test length(a.data) ==  symmetric_tensor_size(N, dim)
+                    @test length(a) == N^dim
+                    @test sizeof(a) > sizeof(a.data)
+                    @test ndims(a) == dim
+                    @test axes(a,1) == Base.OneTo(N)
+                end
+            end
+        end
+    end
+end
+
+@testset "degeneracy" begin
+    for N in [2, 5, 10]
+        for dim in [2, 5, 6]
+            a = ones(SymmetricTensor{BigInt, N, dim})
+            d = find_degeneracy(a)
+            @test sum(d.data.*a.data) == length(a)
+
+            a = rand(SymmetricTensor{Float64, N, dim})
+            d = find_degeneracy(a)
+            @test sum(a) ≈ sum(d.data.*a.data)
+        end
+    end
+end
+
